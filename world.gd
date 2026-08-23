1ea895e26f69dc49e5f2a333653a2ecd0bd75cc4
Michael Trevizo <michael.trevizo93@gmail.com>
2026-08-22 18:38:50 -0700

    Make camera move

--- world.gd
+++ world.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var camera_speed: int = 100
+
 @onready var camera: Camera2D = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	_handle_cam_movement(delta)
+
+
+func _handle_cam_movement(delta: float) -> void:
+	var velocity = Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down") * camera_speed
+	camera.position += velocity * delta
