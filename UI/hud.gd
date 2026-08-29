3df25533ad0fb26b79d78ff242585ebf7908f589
Michael Trevizo <michael.trevizo93@gmail.com>
2026-08-29 00:40:07 -0700

    Add buttons to UI

--- UI/hud.gd
+++ UI/hud.gd
@@ -1,8 +1,12 @@
 class_name HUD
 extends CanvasLayer
 
-@onready var speed: Label = $MarginContainer/LabelContainer/VBoxContainer/SpeedContainer/Speed
-@onready var zoom: Label = $MarginContainer/LabelContainer/VBoxContainer/ZoomContainer/Zoom
+signal sim_start
+signal sim_step
+signal sim_stop
+
+@onready var speed: Label = $MarginContainer/VBoxContainer/LabelContainer/HBoxContainer/SpeedContainer/Speed
+@onready var zoom: Label = $MarginContainer/VBoxContainer/LabelContainer/HBoxContainer/ZoomContainer/Zoom
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,3 +25,15 @@ func update_speed_label(value: int) -> void:
 
 func update_zoom_label(value: float) -> void:
 	zoom.text = "{value}x".format({"value": "%0.1f" % value})
+
+
+func _on_play_button_pressed() -> void:
+	sim_start.emit()
+
+
+func _on_step_button_pressed() -> void:
+	sim_step.emit()
+
+
+func _on_stop_button_pressed() -> void:
+	sim_stop.emit()
