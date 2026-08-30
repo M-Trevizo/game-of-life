e44ca8959f5198b9d6a06249e15df1eb073ee7cf
Michael Trevizo <michael.trevizo93@gmail.com>
2026-08-29 21:00:28 -0700

    Add documentation

--- Grid/chunk.gd
+++ Grid/chunk.gd
@@ -1,8 +1,15 @@
 class_name Chunk
 extends Node2D
+## Handles all chunk related functionality
+##
+## Represents a square "chunk" of the grid space.
+## Acts as both an instance of each chunk and containes all static variables
+## and static functions needed to handle chunks.
+
 
 #signal generate_chunks(chunk_pos: Vector2i, num_chunks: int)
 
+## Sides of the chunk.
 enum Sides {
 	UP,
 	DOWN,
@@ -10,6 +17,10 @@ enum Sides {
 	RIGHT,
 }
 
+## Side length of each chunk.
+const CHUNK_WIDTH: int = 10 * 50
+
+## Dictionary of all chunks created
 static var chunks: Dictionary = {}
 static var chunk_scene: PackedScene = preload("uid://dbffxqi58ld0")
 static var next_chunk_id: int = 0:
@@ -22,16 +33,22 @@ static var alive_tile_atlas_coords := Vector2i(1, 0)
 static var dead_tile_atlas_coords := Vector2i(0, 0)
 
 var id: int
+
+## The pseudo location of the chunk given in relation to chunk origin (0, 0)
+var location: Vector2i
 var contains_mouse: bool
 var active_cells: Array[Vector2i]
 
 @onready var tilemap_layer: TileMapLayer = $TileMapLayer
 
 
+## Creates a new chunk at the given global position
+## Should always be used to create a new chunk instead of [method Object.new]
 static func create(pos: Vector2i) -> Chunk:
 	var instance: Chunk = chunk_scene.instantiate()
 	instance.id = next_chunk_id
 	instance.position = pos
+	instance.location = Vector2i(pos.x / CHUNK_WIDTH, pos.y / CHUNK_WIDTH)
 	chunks[instance.id] = instance
 	return instance
 
@@ -55,7 +72,7 @@ static func generate_chunks(num_of_chunks: int, side: Sides, starting_pos: Vecto
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and contains_mouse:
-			var local_pos = to_local(get_global_mouse_position())
+			var local_pos: Vector2 = to_local(get_global_mouse_position())
 			var map_pos: Vector2i = tilemap_layer.local_to_map(local_pos)
 			_flip_cell(map_pos)
 			#print(map_pos)
